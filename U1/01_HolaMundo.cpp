24ef167433d77a5ebaa455531cccc42cdb5b039e
UP210246 <https://github.com/UP210246/UP210246_CPP.git>
2022-09-16 11:14:52 -0500

    01

--- U1/01_HolaMundo.cpp
+++ U1/01_HolaMundo.cpp
@@ -1,1 +1,18 @@
+/* Unidad 1. Hola mundo
+   Autor: Luis Ernesto Anaya Tiscareño
+   Fecha: 01/09/2022
+   Descripción: Imprime en pantalla un texto 
+*/
 
+//Libreria para manejo de entradas y salida de pantalla
+#include <iostream>
+
+//Función principal de tipo entero
+int main()
+{
+    //Mediante la instrucción std::cout envia por la pantalla el texto
+    std::cout << "Hola mundo, este es mi primera vez programando\n";
+
+    //Al ser una función debe retornar un valor, en este caso 0
+    return 0;
+}
