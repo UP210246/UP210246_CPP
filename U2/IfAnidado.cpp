0aadb7cd610a0c248a5b0d4081c4a41129802b12
UP210246 <https://github.com/UP210246/UP210246_CPP.git>
2022-09-22 10:31:59 -0500

    change name

---
+++ U2/IfAnidado.cpp
@@ -0,0 +1,29 @@
+/* Unidad 2. Uso del if anidado
+   Autor: Yahir Garcia Alba 
+   Fecha: 22/09/2022
+   Descripción: Muestra el uso del if anidado
+   Una tienda de zapatos tiene una promoción de descuentos para mayoreo. Si son mas de 10 tiene el 10% de descuento, 
+   si es mayor a 20 y menor de 30 tiene el 20% y mayor de 30 el 40% de descuento. Cada zapato cuesta $50
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int zapatosVendidos;
+    int costosZapatos=50;
+    float montoTotal;
+    float descuento;
+
+    //se solicita la cantidad de zapatos vendidos
+    cout <<"Ingrese la cantidad de zapatos vendidos: ";
+    cin >> zapatosVendidos;
+
+    montoTotal= zapatosVendidos*costosZapatos;
+
+    //se calcula el descuento apicable
+    
+
+}
