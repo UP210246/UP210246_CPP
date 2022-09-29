f45abf581c697bc71982b23b7dceb94c468d48dd
UP210246 <https://github.com/UP210246/UP210246_CPP.git>
2022-09-29 09:46:25 -0500

    04por2

--- U2/04_Pizeria.cpp
+++ U2/04_Pizeria.cpp
@@ -1,8 +1,8 @@
 /*Exercise 4. The Bella Napoli pizzeria offers vegetarian and non-vegetarian pizzas to its customers. The ingredients for each type of pizza appear below.
  Vegetarian ingredients: Pepper and tofu.
- Non-vegetarian ingredients: Pepperoni, Ham and Salmon. 
-Write a program that asks the user if he wants a vegetarian pizza or not, and based on his answer shows him a menu with the ingredients available for him to choose from. 
-You can only choose one ingredient besides the mozzarella and tomato that are in all the pizzas. 
+ Non-vegetarian ingredients: Pepperoni, Ham and Salmon.
+Write a program that asks the user if he wants a vegetarian pizza or not, and based on his answer shows him a menu with the ingredients available for him to choose from.
+You can only choose one ingredient besides the mozzarella and tomato that are in all the pizzas.
 At the end it should be shown on the screen if the chosen pizza is vegetarian or not and all the
 ingredients it contains.*/
 
@@ -10,67 +10,77 @@ ingredients it contains.*/
 
 using namespace std;
 
-int main(){
+int main()
+{
 
     int typePizza;
     int ingredientVegetal;
     int ingredientNoVegetal;
-        
-    cout<< "MENU" << endl;
-    //salto de linea
+
+    cout << "MENU" << endl;
+    // salto de linea
     cout << "\n";
 
     cout << "Vegetarian ingredients: Pepper and tofu." << endl;
-    cout << "Non-vegetarian ingredients: Pepperoni, Ham and Salmon." <<endl;
-    
-    cout << "\n";
-
-    //ask about Pizza
-    cout << "Do you want vegetarian pizza?" <<endl;
-    cout << "1.- vegetarian pizza "<< endl;
-    cout << "2.- No vegetarian pizza "<< endl;
+    cout << "Non-vegetarian ingredients: Pepperoni, Ham and Salmon." << endl;
 
     cout << "\n";
 
-    cout << "select the type of pizza"<< endl;
-    cin >> typePizza;   
+    // ask about Pizza
+    cout << "Do you want vegetarian pizza?" << endl;
+    cout << "1.- vegetarian pizza " << endl;
+    cout << "2.- No vegetarian pizza " << endl;
 
-    //line break
+    cout << "\n";
+
+    cout << "select the type of pizza" << endl;
+    cin >> typePizza;
+
+    // line break
     cout << "\n";
 
     cout << "Note choose only one ingredient" << endl;
-    //salto de linea
+    // salto de linea
     cout << "\n";
+
+    if (typePizza == 1)
+    {
+        cout << "Vegetarian ingredients: Pepper and tofu." << endl;
+        cout << "3.-Pepper" << endl;
+        cout << "4.-tofu" << endl;
+        cin >> ingredientVegetal;
+    }
+    else if (typePizza == 2)
+    {
+        cout << "Non-vegetarian ingredients: Pepperoni, Ham and Salmon." << endl;
+        cout << "5.-Peperoni" << endl;
+        cout << "6.-Ham and Salmon" << endl;
+        cin >> ingredientNoVegetal;
+    }
+    else
+    {
+        cout << "ERROR" << endl;
+    }
+    cout << "\n";
+
+    if (ingredientVegetal == 3)
+    {
+        cout << "Your pizza is vegetarian with Mozzarella, Tomato and Peper" << endl;
+    }
+    else if (ingredientVegetal == 4)
+    {
+        cout << "Your pizza is vegetarian with Mozzarella, Tomato and Tofu" << endl;
+    }
     
-    if (typePizza == 1 ){
-    cout << "Vegetarian ingredients: Pepper and tofu." << endl;
-    cout << "3.-Pepper" <<endl;
-    cout << "4.-tofu" <<endl;
-    cin >> ingredientVegetal;
-   }   
-   else if (typePizza == 2 ){
-    cout << "Non-vegetarian ingredients: Pepperoni, Ham and Salmon." <<endl;
-    cout << "5.-Peperoni" <<endl;
-    cout << "6.-Ham and Salmon" <<endl; 
-    cin >> ingredientNoVegetal;   
-   }
+    if (ingredientNoVegetal == 5)
+    {
+        cout << "Your pizza is non-vegetarian Mozzarella, Tomato with pepperoni" << endl;
+    }
+    else if (ingredientNoVegetal == 6)
+    {
+        cout << "Your pizza is not vegetarian Mozzarella, Tomato with Ham and Salmon" << endl;
+    }
     cout << "\n";
 
-    if(ingredientVegetal == 3){
-    cout << "tu piza es de vegetaria con peper" << endl;
-   }  
-    else if(ingredientVegetal == 4){
-    cout << "tu piza es de vegetaria con tofu" << endl;
-   }
-    if(ingredientNoVegetal == 5){
-    cout << "tu piza es de no vegetaria con peperoni" << endl;
-   }     
-    else if(ingredientNoVegetal == 6){
-    cout << "tu piza es de no vegetaria con Ham and Salmon" << endl;
-   }  
-
-
-   
-   cout << "\n";
-
+    return 0;
 }
