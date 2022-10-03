3a8b3816d7fbd2911acfa0472cf40a500b7f3c32
UP210246 <https://github.com/UP210246/UP210246_CPP.git>
2022-10-03 10:23:42 -0500

    05

---
+++ U2/05_Averge.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int contador = 1;
+    float temperatura;
+    float tempAcum = 0;
+    float temMax = 0;
+    float temMin = 9999;
+    do
+    {
+        cout << "give me the temperature: ";
+        cin >> temperatura;
+        tempAcum += temperatura;
+        contador ++;
+        if (temperatura <= temMin )
+            temMin = temperatura;
+        if (temperatura >= temMax )
+            temMax = temperatura;
+    } while (contador <= 6);
+
+    cout << "\n";
+    cout << "The avergange of temperature today is : " << tempAcum/6 << endl;
+    cout << "\n";
+    cout << "The avergange of temperature maximum today is : " << temMax << endl;
+    cout << "\n";
+    cout << "The avergange of temperature minimal today is : " << temMin << endl;
+    cout << "\n";
+return 0;
+    
+}
